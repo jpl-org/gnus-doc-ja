3e5b24a9a4ee7f2f5bc678f5dba51de139787dd1
Katsumi Yamaoka <yamaoka@jpl.org>
2016-01-05 01:21:02 +0000

    Update copyright year to 2016

--- infohack.el
+++ infohack.el
@@ -1,5 +1,5 @@
 ;;; infohack.el --- a hack to format info file.
-;; Copyright (C) 2001-2015 Free Software Foundation, Inc.
+;; Copyright (C) 2001-2016 Free Software Foundation, Inc.
 
 ;; Author: Shenghuo Zhu <zsh@cs.rochester.edu>
 ;; Keywords: info
